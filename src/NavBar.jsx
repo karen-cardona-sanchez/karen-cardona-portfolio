b8c51e3cb8dda7749fa7b8d33382735f30af0d6f
Karen Cardona <karencs1105@gmail.com>
2025-06-17 10:43:15 -0400

    Editing links in NavBar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -9,7 +9,7 @@ export function NavBar() {
                 <li className="nav-link"><Link to="/">Home</Link></li>
                 <li className="nav-link"><Link to="/pets">Pets</Link></li>
                 <li className="nav-link"><Link to="/hobbies">Hobbies</Link></li>
-                <li className="nav-link"><Link t0='./favorites.html'>Favorites</a></li>
+                <li className="nav-link"><Link t0='./favorites.html'>Favorites</Link></li>
                 <li className="nav-link"><a href='karencardonaportfolio.netlify.app/resume'>Resume</a></li>
             </ul>
         </nav>
