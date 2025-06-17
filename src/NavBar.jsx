fc0b624c838cd1c006ce6ff19271f49fcfd03c35
Karen Cardona <karencs1105@gmail.com>
2025-06-17 10:42:23 -0400

    change links in navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,12 +1,16 @@
+
+
+import {Link} from "react-router-dom"
+
 export function NavBar() {
     return (
         <nav>
             <ul>
-                <li className="nav-link"><a href="/">Home</a></li>
-                <li className="nav-link"><a href="/pets">Pets</a></li>
-                <li className="nav-link"><a href="/hobbies">Hobbies</a></li>
-                <li className="nav-link"><a href='./favorites.html'>Favorites</a></li>
-                <li className="nav-link"><a href='./resume.html'>Resume</a></li>
+                <li className="nav-link"><Link to="/">Home</Link></li>
+                <li className="nav-link"><Link to="/pets">Pets</Link></li>
+                <li className="nav-link"><Link to="/hobbies">Hobbies</Link></li>
+                <li className="nav-link"><Link t0='./favorites.html'>Favorites</a></li>
+                <li className="nav-link"><a href='karencardonaportfolio.netlify.app/resume'>Resume</a></li>
             </ul>
         </nav>
 
